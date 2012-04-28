9cbf353e6772d6534bc510404a7c59f31e97008f
Etienne Garnier <garnier.etienne@gmail.com>
2012-04-28 15:25:16 +0200

    init the rails app

---
+++ README.md
@@ -0,0 +1,6 @@
+Webmail
+=======
+
+This app is designed to be a simple webmail capable to connect to popular email service providers through IMAP protocol.
+
+__Warning__: This app is still in development and is not ready for use.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webmail::Application.config.secret_token = '63fb3c8a1eae877dd7842cc2b9f2bf2219c7b6f2481bc476429cecad7828f24c37bb306d83ce817243bf469ab1b62455cb586ba06dc597b504cf23d9562324d1'
