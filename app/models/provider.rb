58f80cb1e4a4d6bed9311bbe4903a1a7cdd3ce11
Etienne Garnier <garnier.etienne@gmail.com>
2012-04-28 19:22:56 +0200

    add presence validation and testing for Provider's attributes

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -5,8 +5,18 @@
 #   gmail.imap_address => 'imap.gmail.com'
 #
 # These data are seeded into the db/seeds.rb file.
+# Default values for imap_port and imap_ssl are defined in the migration file.
+#   imap_port: 993
+#   imap_ssl:  true
 class Provider < ActiveRecord::Base
   attr_accessible :imap_address, :imap_port, :imap_ssl, :name
 
+  # A Provider serve many Accounts
+  has_many :accounts
+
+  # The name and IMAP settings (address, port and SSL) must be specified
+  validates :name, :imap_address, :imap_port, :imap_ssl, presence: true
+
+  # The Provider name must be unique
   validates :name, uniqueness: true
 end
--- test/unit/provider_test.rb
+++ test/unit/provider_test.rb
@@ -2,6 +2,28 @@ require 'test_helper'
 
 class ProviderTest < ActiveSupport::TestCase
 
+  test "provider name must be present" do
+  	provider = Provider.new(imap_address: 'imap.test.com', imap_port: 993, imap_ssl: true)
+  	assert !provider.valid?, "a provider without a name must not be valid"
+  end
+
+  test "an imap server address must be present" do
+  	provider = Provider.new(name: 'test.com', imap_port: 993, imap_ssl: true)
+  	assert !provider.valid?, "a provider without an IMAP address must not be valid"
+  end  
+  
+  test "an imap server port must be present" do
+  	provider = Provider.new(name: 'test.com', imap_address: 'imap.test.com', imap_ssl: true)
+  	assert provider.valid?, "no default value are enabled for the IMAP port"
+  	assert_equal provider.imap_port, 993
+  end
+
+  test "an imap server SSL indicator must be present" do
+  	provider = Provider.new(name: 'test.com', imap_address: 'imap.test.com', imap_port: 993)
+  	assert provider.valid?, "no default value are enabled for the SSL indicator"
+  	assert provider.imap_ssl, "the default value must be true"
+  end
+
   test "provider name must be unique" do
   	provider1 = Provider.new(name: 'test.com', imap_address: 'imap.test.com', imap_port: 993, imap_ssl: true)
   	assert provider1.save, 'this provider must be correctly saved'
