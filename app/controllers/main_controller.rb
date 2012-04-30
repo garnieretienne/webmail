dde8feec4666441ae00b785a318f852cf6c7435c
Etienne Garnier <garnier.etienne@gmail.com>
2012-04-30 04:32:28 +0200

    login page go to the Sessions controller, no more need for the Main controller

--- app/controllers/main_controller.rb
+++
@@ -1,7 +0,0 @@
-# Control the welcome and the login pages
-class MainController < ApplicationController
-
-  # Login page
-  def login
-  end
-end
--- test/functional/main_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MainControllerTest < ActionController::TestCase
-
-  test "should get login" do
-    get :login
-    assert_response :success
-  end
-end
