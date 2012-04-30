89a16600b641a5903a1674cd57c92bcfc474427d
Etienne Garnier <garnier.etienne@gmail.com>
2012-04-30 16:38:58 +0200

    add current_account helper, authenticate filter and footer to the layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Helper to return the current authenticated account
+  def current_account
+    Account.find(session[:account_id]) if session[:account_id]
+  end
+  helper_method :current_account
+
+  # Filter to manage authorization
+  #   before_filter :authenticate
+  def authenticate
+    redirect_to login_path, alert: "You must be authenticated" if !current_account
+  end
+end
 end
